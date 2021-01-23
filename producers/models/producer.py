b5edf713e6358e94caba0bed65b028bea5bfb680
Gutierrez, Idelfonso <igutierrez@ulta.com>
2021-01-23 17:06:27 +0300

    I'm Batman (this commit has no parent)

---
+++ producers/models/producer.py
@@ -0,0 +1,68 @@
+"""Producer base-class providing common utilites and functionality"""
+import logging
+import time
+
+
+from confluent_kafka import avro
+from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka.avro import AvroProducer, CachedSchemaRegistryClient
+
+logger = logging.getLogger(__name__)
+
+KAFKA_BROKER_URL = "localhost:9092"
+SCHEMA_REGISTRY_URL = "localhost:8081"
+
+class Producer:
+    """Defines and provides common functionality amongst Producers"""
+    existing_topics = set([])
+
+    def __init__(
+        self,
+        topic_name,
+        key_schema,
+        value_schema=None,
+        num_partitions=1,
+        num_replicas=1,
+    ):
+        """Initializes a Producer object with basic settings"""
+        self.topic_name = topic_name
+        self.key_schema = key_schema
+        self.value_schema = value_schema
+        self.num_partitions = num_partitions
+        self.num_replicas = num_replicas
+
+        self.broker_properties = {"bootstrap.servers": KAFKA_BROKER_URL}
+
+        if self.topic_name not in Producer.existing_topics:
+            self.create_topic()
+            Producer.existing_topics.add(self.topic_name)
+
+        schema_registry = CachedSchemaRegistry(SCHEMA_REGISTRY_URL)
+
+        self.producer = AvroProducer(
+            self.broker_properties,
+            default_key_schema=self.key_schema,
+            default_value_schema=self.value_schema,
+            schema_registry=schema_registry)
+
+    def create_topic(self):
+        """Creates the producer topic if it does not already exist"""
+        client = AdminClient(self.broker_properties)
+
+        topic = NewTopic(
+            self.topic_name, 
+            num_partitions=self.num_partitions, 
+            replication_factor=self.num_replicas)
+
+        client.create_topics([topic])
+
+    def time_millis(self):
+        return int(round(time.time() * 1000))
+
+    def close(self):
+        Producer.existing_topics = set([])
+        self.producer.close()
+
+    def time_millis(self):
+        """Use this function to get the key for Kafka Events"""
+        return int(round(time.time() * 1000))
